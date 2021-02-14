510339a9e2b683ae3fff919b3f57585e0dcdf80b
PavelTinyakov <tinyakov.p.g@yandex.ru>
2021-02-14 19:48:53 +0300

    update task_3

--- lesson_4/task_3.sql
+++ lesson_4/task_3.sql
@@ -1,97 +1,29 @@
--- Создаём БД
-CREATE DATABASE vk;
-
--- Убрал изначальные комментарии. Присутсвуют только комментарии по изменениям.
-
--- Делаем её текущей
-USE vk;
-
--- Создаём таблицу пользователей
-CREATE TABLE users (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  phone VARCHAR(100) NOT NULL UNIQUE,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,  
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);  
-
--- Таблица профилей
-CREATE TABLE profiles (
-  user_id INT UNSIGNED NOT NULL PRIMARY KEY , 
-  gender CHAR(1) NOT NULL,
-  birthday DATE,
-  city VARCHAR(130),
-  country VARCHAR(130),
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,  
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-); 
-
--- Таблица сообщений
-CREATE TABLE messages (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  from_user_id INT UNSIGNED NOT NULL,
-  to_user_id INT UNSIGNED NOT NULL,
-  body TEXT NOT NULL,
-  is_important BOOLEAN DEFAULT FALSE COMMENT "добавил значение по умолчанию",
-  is_delivered BOOLEAN DEFAULT FALSE COMMENT "добавил значение по умолчанию",
-  created_at DATETIME DEFAULT NOW()
-) COMMENT "Есть изменения";
-
--- Таблица дружбы
-CREATE TABLE friendship (
-  user_id INT UNSIGNED NOT NULL,
-  friend_id INT UNSIGNED NOT NULL,
-  friendship_status ENUM ('приятель', 'знакомый', 'друг', 'лучший друг') DEFAULT '' COMMENT "Если статусы ограничены, то как вариант реализации",
-  requested_at DATETIME DEFAULT NOW(),
-  confirmed_at DATETIME,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,  
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  
-  PRIMARY KEY (user_id, friend_id)
-) COMMENT "Есть изменения";
-
--- Таблица статусов дружеских отношений УДАЛЕНА!
-
-
--- Таблица групп
-CREATE TABLE communities (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  owner_id INT UNSIGNED NOT NULL COMMENT "Добавил владельца(создателя) группы",
-  name VARCHAR(150) NOT NULL COMMENT "Убрал UNIQUE",
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,  
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  UNIQUE (owner_id, name) COMMENT "Сделал составное ограничение"
-) COMMENT "Есть изменения";
-
--- Таблица связи пользователей и групп
-CREATE TABLE communities_users (
-  community_id INT UNSIGNED NOT NULL,
-  user_id INT UNSIGNED NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
-  PRIMARY KEY (community_id, user_id)
+-- таблица лайков/дизлайков
+CREATE TABLE likes (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Индетификатор строки',
+  from_user_id INT UNSIGNED NOT NULL COMMENT 'id юзера кто ставит лайк',
+  object_id INT UNSIGNED NOT NULL COMMENT 'id категории объекта из таблицы likes_objects, например messages',
+  in_object_id INT UNSIGNED NOT NULL COMMENT 'id конкретного объекта внутри категории',
+  like_dis ENUM (1, -1, 0) NOT NULL DEFAULT 0 COMMENT '1 - лайк, -1 - дизлайк, 0 - возможность отменить лайка/дизлайка',
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE (from_user_id, oblect_id, in_object_id) COMMENT 'обязательно уникальное сочетание'
+);
+-- таблица категорий объектов участвующих в лайках
+CREATE TABLE likes_objects (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT;
+  name VARCHAR (255) NOT NULL UNIQUE,
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- Таблица медиафайлов
-CREATE TABLE media (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT UNSIGNED NOT NULL,
-  filename VARCHAR(255) NOT NULL,
-  size INT NOT NULL,
-  metadata JSON,
-  media_type_id INT UNSIGNED NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+-- таблица постов
+CREATE TABLE posts (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Индетификатор строки',
+  owner_id INT UNSIGNED NOT NULL COMMENT 'создатель поста',
+  subject VARCHAR (255) NOT NULL COMMENT 'тема/заголовок поста',
+  message TEXT NOT NULL COMMENT 'текст',
+  media_id INT UNSIGNED NOT NULL COMMENT 'ссылка на медиафайл'
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
--- Таблица типов медиафайлов
-CREATE TABLE media_types (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL UNIQUE,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,  
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-
-
- 
