4ac2e8fc14d2530086e56f8bf5172b5623d655a3
PavelTinyakov <tinyakov.p.g@yandex.ru>
2021-02-18 01:43:30 +0300

    update task_2_2

--- lesson_5/task_2_2.sql
+++ lesson_5/task_2_2.sql
@@ -1,4 +1,4 @@
 SELECT 
   COUNT(*) AS score, 
-  DAYNAME(DATE(CONCAT_WS('-', YEAR(NOW()), MONTH(birthday_at), DAY(birthday_at)))) AS dayweek FROM users
+  DAYNAME(CONCAT_WS('-', YEAR(NOW()), MONTH(birthday_at), DAY(birthday_at))) AS dayweek FROM users
 GROUP BY dayweek;
